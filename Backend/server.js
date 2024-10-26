355dfe6c5b8f38fadc5730cca71a646ab801bece
omkar R <93393539+omk-coder@users.noreply.github.com>
2024-10-26 13:28:28 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,8 +13,9 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL],
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
     optionsSuccessStatus: 200
 }));
 
